59f7ea71760976c1c685e371ea2cacf0bcd7866f
Pete A <peter.ayresmusic@gmail.com>
2018-02-27 20:34:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -39,7 +39,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
   return array
 }
 function removeElementFromEndOfArray(array){
-  array.slice(0)
-  array.length(2)
+  array.slice[0]
+  array.length[2]
   return array
 }
