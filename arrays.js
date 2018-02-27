f4e0c321ac4c61f503adaca0567d252bac5dca02
Pete A <peter.ayresmusic@gmail.com>
2018-02-27 21:22:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,7 +21,7 @@ function destructivelyAddElementToEndOfArray(array, element){
 
 function accessElementInArray(array,index){ 
   array[0];
-  return 
+  return index 
 } 
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
